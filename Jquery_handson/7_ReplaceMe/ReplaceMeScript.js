8c20ec412476d5ac5a1c581ab47576f366b6a7c2
yashoza20 <59004361+yashoza20@users.noreply.github.com>
2021-02-13 12:34:58 +0530

    replace the word with smilie emoji

---
+++ Jquery_handson/7_ReplaceMe/ReplaceMeScript.js
@@ -0,0 +1,7 @@
+function replaceMethod(){
+    $("#p1").append($("#ta").val()).css({"background-color":"crimson"});
+}
+function repMethod(){
+    $( "#p1" ).replaceWith( $("#d2") );
+    $("#d2").show();
+}
